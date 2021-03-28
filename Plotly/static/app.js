79f83ff8a509fc94900e0975e9d53a32809fabb7
Yvonne Tran <petertran@Yvonnes-MacBook-Pro.local>
2021-03-27 22:08:09 -0700

    So many Updates

--- Plotly/static/app.js
+++ Plotly/static/app.js
@@ -1,86 +1,159 @@
-// d3.selectAll("select").on("change", function() {
-//     // What will be logged out? What is `this` in this case?
-//     d3.json("../data/samples.json").then((importedData) => {
-//             // console.log(importedData);
-//             var data = importedData;
-//             data.map(row => row.names)
-//             console.log(row.names)
-//             // Sort the data array using the greekSearchResults value
-//             // data.sort(function(a, b) {
-//             //   return parseFloat(b.sample_values) - parseFloat(a.sample_values);
-//             // });
-          
-//             // // Slice the first 10 objects for plotting
-//             // data = data.slice(0, 10);
-          
-//             // // Reverse the array due to Plotly's defaults
-//             // data = data.reverse();
-          
-//             // // Trace1 for the Greek Data
-//             // var trace1 = {
-//             //   x: data.map(row => row.sample_values),
-//             //   y: data.map(row => row.otu_ids),
-//             //   text: data.map(row => row.otu_labels),
-//             // //   name: "Greek",
-//             //   type: "bar",
-//             //   orientation: "h"
-//             // };
-//     // Answer: It will console log the `button` element.
-//   });})
+var sample_data 
 d3.json("../data/samples.json").then(function(importedData) {
+    sample_data = importedData
+
     var select = d3.select("#selDataset")
-    // select.append("option")
-    importedData.names.forEach((value, index)=>{
-        console.log(value)
+
+    var samples = sample_data.samples
+
+    var sample_id = samples.map(row => row.id)
+
+    sample_id.forEach((value, index)=>{
+
         select.append("option").text(value).property("value", index)
     })
-
-    importedData.metadata[0]
-    console.log(importedData.names)
+    update(0)
+    updateBar(0)
+    sorryToBurstYour(0)
 })
 
-.onChange
+var sample_index
+var sample_values
+var data
+var metavalue
+var otu_ids
+var otu_labels
 
-// d3.json("data/data.json").then((importedData) => {
-//     // console.log(importedData);
-//     var data = importedData;
+function update(value){
+  sample_index = sample_data.samples[value]
+
+  sample_values = sample_index.sample_values
+  otu_ids = sample_index.otu_ids
+  otu_labels = sample_index.otu_labels
+  // otu_ids = otu_ids.toString();
+
+  sample_values.sort(function(a, b){
+    return parseFloat (b) - parseFloat(a)
+});
+
+data = sample_values.slice(0, 10);
+// Reverse the array due to Plotly's defaults
+data  = data.reverse();
+
+metavalue = sample_data.metadata[value]
+
+var meta = d3.selectAll("#sample-metadata");
+
+meta.html("");
+
+Object.entries(metavalue).forEach(([key, value])=>{
   
-//     // Sort the data array using the greekSearchResults value
-//     data.sort(function(a, b) {
-//       return parseFloat(b.sample_values) - parseFloat(a.sample_values);
-//     });
+  var demInfo = meta.append("div");
+  // meta.html(`<div class="panel-body">${key}: ${value}</div>`)
+  demInfo.text(`${key}: ${value}`)
+})
+// updateBar(value)
+}
+
+function updateBar(sickBars){
+var trace1 = {
+  x: data,
+  y: `OTU ${otu_ids}`,
+  // y: "Content",
+  text: otu_labels,
+  name: "Bar Graph",
+  type: "bar",
+  orientation: "h"
+};
+
+// data
+var barData = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Bar Chart"
+}
+
+Plotly.newPlot("bar", barData, layout);
+}
+
+function sorryToBurstYour(bubble){
+  var trace1 = {
+    x: otu_ids,
+    y: sample_values,
+    mode: 'markers',
+    marker: {
+      size: sample_values,
+      color: otu_ids
+    },
+    text: otu_labels
+  };
   
-//     // Slice the first 10 objects for plotting
-//     data = data.slice(0, 10);
+  var bubData = [trace1];
   
-//     // Reverse the array due to Plotly's defaults
-//     data = data.reverse();
+  var layout = {
+    title: 'Bubble Chart',
+    xaxis: {title: "OTU ID"}
+    // showlegend: false,
+    // height: 600,
+    // width: 600
+  };
   
-//     // Trace1 for the Greek Data
-//     var trace1 = {
-//       x: data.map(row => row.sample_values),
-//       y: data.map(row => row.otu_ids),
-//       text: data.map(row => row.otu_labels),
-//     //   name: "Greek",
-//       type: "bar",
-//       orientation: "h"
-//     };
+  Plotly.newPlot('bubble', bubData, layout);
+}
+
+// function dirtyBellyGauge(gross){
   
-//     // data
-//     var chartData = [trace1];
-  
-//     // Apply the group bar mode to the layout
-//     var layout = {
-//       title: "Greek gods search results",
-//       margin: {
-//         l: 100,
-//         r: 100,
-//         t: 100,
-//         b: 100
+//   // part of data to input
+//   var traceGauge = {
+//     type: 'pie',
+//     showlegend: false,
+//     hole: 0.4,
+//     rotation: 90,
+//     values: [ 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+//     text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+//     direction: 'clockwise',
+//     textinfo: 'text',
+//     textposition: 'inside',
+//     marker: {
+//       colors: ['','','','','','','','','','white'],
+//       labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+//       hoverinfo: 'label'
+//     }
+//   }
+
+//   // needle
+//   var degrees = 50, radius = .9
+//   var radians = degrees * Math.PI / 180
+//   var x = -1 * radius * Math.cos(radians) * wfreqNum
+//   var y = radius * Math.sin(radians)
+
+//   var gaugeLayout = {
+//     shapes: [{
+//       type: 'line',
+//       x0: 0.5,
+//       y0: 0.5,
+//       x1: 0.6,
+//       y1: 0.6,
+//       line: {
+//         color: 'black',
+//         width: 3
 //       }
-//     };
-  
-//     // Render the plot to the div tag with id "plot"
-//     Plotly.newPlot("plot", chartData, layout);
-//   });
-  
+//     }],
+//     title: 'Chart',
+//     xaxis: {visible: false, range: [-1, 1]},
+//     yaxis: {visible: false, range: [-1, 1]}
+//   }
+
+//   var dataGauge = [traceGauge]
+
+//   Plotly.plot('gauge', dataGauge, gaugeLayout)
+// }
+
+
+
+function optionChanged(value){
+  update(value)
+  updateBar(value)
+  sorryToBurstYour(value)
+  }
